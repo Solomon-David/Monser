aaf8311e7689f5f5abb7b73cab0081ff08eb9bb7
Solomon-David <sakesobia@yahoo.com>
2022-07-11 19:40:56 +0100

    updating mongoose uri

--- mongos.js
+++ mongos.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-const url="mongodb://127.0.0.1:27017"
+const url=process.env.MONGODB_URI|| "mongodb://127.0.0.1:27017"
 const dbname="profiles"
 
 mongoose.connect(`${url}/${dbname}`, {useNewUrlParser:true})
